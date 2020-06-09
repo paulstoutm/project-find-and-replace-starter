94432060cae2152f2d4f16e0241423603fc9d57c
unknown <paul@paulstoutcountry.com>
2020-06-09 08:53:31 -0400

    final commit

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
 
-const findInput = document.querySelector(".find-input")
-const replaceInput = document.querySelector(".replace-input")
-const replaceAllButton = document.querySelector(".replace-all-button")
 
-// The following variable holds your OUTER ARRAY of row elements.
-// Later you will need an OUTER LOOP to loop over the individual elements within
-// this array.
-const rowElements = document.querySelectorAll(".row")
+let findInput = document.querySelector(".find-input")
+let replaceInput = document.querySelector(".replace-input")
+let replaceAllButton = document.querySelector(".replace-all-button")
+let rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
-// containing the cell elements for a given row.
-// Call this function from WITHIN your row elements loop. Then you will, in turn,
-// need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
+
+function getCellElements(currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
+
 }
+replaceAllButton.addEventListener("click", function () {
+    let userInput = findInput.value
+    let replaceUserInput = replaceInput.value
+    let numberFound = 0
+    // console.log(replaceUserInput)
+    for (let sc = 0; sc < rowElements.length; sc = sc + 1) {
+        let currentRowElement = rowElements[sc]
+        let currentRowElementCellElements = getCellElements(currentRowElement)
+
+        for (let sc2 = 0; sc2 < currentRowElementCellElements.length; sc2 = sc2 + 1) {
+            let currentCellElement = currentRowElementCellElements[sc2]
+            let currentCellItems = currentCellElement.innerHTML
 
 
-// YOUR CODE GOES HERE
+            if (currentCellItems.includes(userInput)) {
+                // console.log("Found", currentCellItems)
+                currentCellElement.innerHTML = currentCellItems.replace(userInput, replaceUserInput)
+                numberFound = numberFound + 1
+                console.log(numberFound)
+                currentCellElement.style.color = "red"
+            } else {
+
+            }
+        }
+    }
+})
+
+
+
+
+
 
 
 // One last thing: dedicate very careful attention to using variables and
